aa696d802355723936035fea15dc8d8d9d2afd2c
Mathesh08-pc <Mathesh08-pc@users.noreply.github.com>
2026-05-08 18:33:20 +0000

    deploy: 62874abafd09fc53158541ad5dd1088ceb483843

--- supabase.js
+++ supabase.js
@@ -2,4 +2,4 @@ const SUPABASE_URL = 'https://xeugngazneqaemamzzmb.supabase.co';
 const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhldWduZ2F6bmVxYWVtYW16em1iIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzgyNTQ5MTAsImV4cCI6MjA5MzgzMDkxMH0.EMoZTfmN1S6re92YZOugm_3mkcb_x6UUpNESCKY9G90';
 
 // Initialize Supabase Client
-const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
+const supabaseClient = window.supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
